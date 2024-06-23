586dc3e5175601df939b0ceaa9c37fc77676d82d
franck <carnet.franck.paul@gmail.com>
2024-06-23 09:13:05 +0200

    Compile l10n (en, fr)

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['User personal memo'] = 'Mémo personnel';
+L10n::$locales['memo'] = 'mémo';
