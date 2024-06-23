926e7c1e4f4c125a952ef2eee26ca9d2c94cc55e
franck <carnet.franck.paul@gmail.com>
2024-06-23 11:26:02 +0200

    l10n

--- locales/en/main.lang.php
+++ locales/en/main.lang.php
@@ -12,3 +12,7 @@ use Dotclear\Helper\L10n;
 
 L10n::$locales['User personal memo'] = '';
 L10n::$locales['memo'] = '';
+L10n::$locales['Memo'] = '';
+L10n::$locales['Content:'] = '';
+L10n::$locales['Number of rows:'] = '';
+L10n::$locales['The contents of your memo will be saved at the same time as the entry.'] = '';
--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -12,3 +12,7 @@ use Dotclear\Helper\L10n;
 
 L10n::$locales['User personal memo'] = 'Mémo personnel';
 L10n::$locales['memo'] = 'mémo';
+L10n::$locales['Memo'] = 'Mémo';
+L10n::$locales['Content:'] = 'Contenu :';
+L10n::$locales['Number of rows:'] = 'Nombre de lignes :';
+L10n::$locales['The contents of your memo will be saved at the same time as the entry.'] = 'Le contenu de votre mémo sera sauvegardé en même temps que l\'entrée (page/billet).';
